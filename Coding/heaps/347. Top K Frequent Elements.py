0358e6a5521b93f8933789e822deb15643783aa4
vidhya131 <vidhyareddy131@gmail.com>
2026-02-05 23:41:01 -0500

    neetcode 150 - 02-05-2026

--- Coding/heaps/347. Top K Frequent Elements.py
+++ Coding/heaps/347. Top K Frequent Elements.py
@@ -17,5 +17,26 @@ class Solution:
         
         return res
         
+# Counting sort or bucket sort solution
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Count of each element
+        m = {}
+        max_freq = 0
+        for i in nums:
+            m[i] = 1+m.get(i,0)
+            max_freq = max(max_freq, m[i])
+        
+        freq = [[] for i in range(max_freq+1)]
+        for key,val in m.items():
+            freq[val].append(key)
+        res = []
+        for i in range(max_freq,-1,-1):
+            res.extend(freq[i])
+            if len(res)>=k:
+                return res[:k]
+        
+
+
 
 
