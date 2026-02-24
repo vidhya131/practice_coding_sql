2019f20353e0733d91fdc518cfbb2ff0b44afff1
vidhya131 <vidhyareddy131@gmail.com>
2026-02-24 16:24:10 -0500

    optimized Coding/sliding_window/76. Minimum Window Substring.py

--- Coding/sliding_window/76. Minimum Window Substring.py
+++ Coding/sliding_window/76. Minimum Window Substring.py
@@ -1,49 +1,32 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        # declarations
-        n = len(t)
-        m = len(s)
-        if m<n:
+        m, n = len(s), len(t)
+        if m < n:
             return ""
-        m1 = {}
+        countT, window = {}, {}
         for i in range(n):
-            m1[t[i]] = m1.get(t[i], 0)+1
-        l = 0
-        m2 = {}
-        min_win = m+1
-        min_substring = ""
+            countT[t[i]] = countT.get(t[i], 0) + 1
+        min_win, min_substring = m + 1, [-1,-1]
+        l, have, need = 0, 0, len(countT)
+        for r in range(m):
+            c = s[r]
+            if c in countT:
+                window[c] = window.get(c, 0)+1
+                if window[c] == countT[c]:
+                    have += 1
+                while have == need:
+                    if min_win > r-l+1:
+                        min_win = r-l+1
+                        min_substring = [l,r]
+                    # remove the left chars to make the substr minimum
+                    if s[l] in countT:
+                        window[s[l]] -= 1
+                        if countT[s[l]] > window[s[l]]:
+                            have -= 1
+                    l += 1
 
-        # logic
-        for i in range(n-1):
-            m2[s[i]] = m2.get(s[i], 0)+1
-        r = n-1
-        while(l<=r and r<m and (r-l+1)>= n):
-            m2[s[r]] = m2.get(s[r], 0)+1
-            # move the left pointer towards right pointer
-            char = None
-            while self.match(m1, m2, char):
-                if min_win>r-l+1:
-                    min_win = r-l+1
-                    min_substring = s[l:r+1]
-                m2[s[l]] = m2.get(s[l])-1
-                if m2[s[l]] == 0:
-                    del m2[s[l]]
-                char = s[l]
-                l += 1
-            # move towards right
-            r = r+1
-        return min_substring
+        return s[min_substring[0]:min_substring[1]+1]
 
+                    
 
-    def match(self, m1, m2, char):
-        if char is not None:
-            m2[char] = m2.get(char, 0) + 1
-            for key, value in m1.items():
-                if m2.get(key, 0) < value:
-                    return False
-            return True
-        else:
-            for key, value in m1.items():
-                if m2.get(key, 0) < value:
-                    return False
-            return True
+            
