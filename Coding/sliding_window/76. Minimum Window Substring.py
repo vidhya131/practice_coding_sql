5fb4843032146e38d89e58b51efde3ca0cc3fec4
vidhya131 <vidhyareddy131@gmail.com>
2026-02-24 11:48:16 -0500

    optimized Coding/sliding_window/76. Minimum Window Substring.py

--- Coding/sliding_window/76. Minimum Window Substring.py
+++ Coding/sliding_window/76. Minimum Window Substring.py
@@ -1,23 +1,27 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        m1 = {}
+        # declarations
         n = len(t)
         m = len(s)
         if m<n:
             return ""
+        m1 = {}
         for i in range(n):
             m1[t[i]] = m1.get(t[i], 0)+1
         l = 0
         m2 = {}
         min_win = m+1
         min_substring = ""
+
+        # logic
         for i in range(n-1):
             m2[s[i]] = m2.get(s[i], 0)+1
         r = n-1
         while(l<=r and r<m and (r-l+1)>= n):
             m2[s[r]] = m2.get(s[r], 0)+1
             # move the left pointer towards right pointer
-            while self.match(m1, m2):
+            char = None
+            while self.match(m1, m2, char):
                 if min_win>r-l+1:
                     min_win = r-l+1
                     min_substring = s[l:r+1]
@@ -30,8 +34,15 @@ class Solution:
         return min_substring
 
 
-    def match(self, m1, m2):
-        for key, value in m1.items():
-            if m2.get(key, 0) < value:
-                return False
-        return True
+    def match(self, m1, m2, char):
+        if char is not None:
+            m2[char] = m2.get(char, 0) + 1
+            for key, value in m1.items():
+                if m2.get(key, 0) < value:
+                    return False
+            return True
+        else:
+            for key, value in m1.items():
+                if m2.get(key, 0) < value:
+                    return False
+            return True
