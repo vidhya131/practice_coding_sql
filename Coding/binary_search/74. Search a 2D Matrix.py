9d414f5eb2458437aadb94dd255cd89a4e88c3c1
vidhya131 <vidhyareddy131@gmail.com>
2026-04-27 14:00:13 -0400

    coding 04-27-2026 improvements

--- Coding/binary_search/74. Search a 2D Matrix.py
+++ Coding/binary_search/74. Search a 2D Matrix.py
@@ -1,23 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        m = len(matrix)
-        n = len(matrix[0])
-        t = m*n
-        l = 0
-        r = t-1
-        while(l<=r and r<t):
-            mid = (l+r)//2
-            mid_el = self.get(mid, m, n, matrix)
-            if  mid_el == target:
-                return True
-            elif mid_el < target:
-                l = mid+1
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if list1 and list2:
+            if list1.val>list2.val:
+                sorted_list_head = list2
+                list2 = list2.next
             else:
-                r  = mid-1
-        return False
+                sorted_list_head = list1
+                list1 = list1.next
+        elif list1:
+            return list1
+        else: # this case if for non-empty list2  or both empty
+            return list2
 
-    def get(self, index, rows, cols, matrix):
-        ## key logic
-        r = index//cols
-        c = index%cols
-        return matrix[r][c]
+        sorted_list = sorted_list_head
+        while(list1 and list2):
+            if list1.val<list2.val:
+                sorted_list.next = list1
+                list1 = list1.next
+            else:
+                sorted_list.next = list2
+                list2 = list2.next
+            sorted_list = sorted_list.next
+
+        sorted_list.next = list1 if list1 else list2
+        return sorted_list_head
+            
+        
