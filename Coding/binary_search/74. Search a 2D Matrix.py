84cfc739f94b7cd7cd84262ee5c9693fe5419e5e
vidhya131 <vidhyareddy131@gmail.com>
2026-04-27 14:03:53 -0400

    coding 04-27-2026 improvements- dummy node version

--- Coding/binary_search/74. Search a 2D Matrix.py
+++ Coding/binary_search/74. Search a 2D Matrix.py
@@ -3,31 +3,24 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if list1 and list2:
-            if list1.val>list2.val:
-                sorted_list_head = list2
-                list2 = list2.next
-            else:
-                sorted_list_head = list1
-                list1 = list1.next
-        elif list1:
-            return list1
-        else: # this case if for non-empty list2  or both empty
-            return list2
 
-        sorted_list = sorted_list_head
-        while(list1 and list2):
-            if list1.val<list2.val:
-                sorted_list.next = list1
+        dummy = ListNode()
+        tail = dummy
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                tail.next = list1
                 list1 = list1.next
             else:
-                sorted_list.next = list2
+                tail.next = list2
                 list2 = list2.next
-            sorted_list = sorted_list.next
 
-        sorted_list.next = list1 if list1 else list2
-        return sorted_list_head
-            
-        
+            tail = tail.next
+
+        # attach remaining nodes (already sorted)
+        tail.next = list1 if list1 else list2
+
+        return dummy.next
